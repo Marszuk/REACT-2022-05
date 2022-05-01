d2f732b341154081e082a934c52674752bf4902a
Marszuk <mariola@szukielojc.com>
2022-05-01 10:46:50 +0200

    change2

--- src/components/Hot.jsx
+++ src/components/Hot.jsx
@@ -1,20 +1,22 @@
 import React from "react"
+import { useDispatch } from "react-redux"
+import { useSelector } from "react-redux"
 import Mem from './Mem'
-import { MemReducer } from "../store/reducers/globalReducer";
-import { useDispatch } from "react-redux";
+import { MemReducer } from "../store/reducers/globalReducer"
+import { selectMem } from "../store/reducers/globalReducer"
 
 
 
-export default class Hot extends React.Component {
-constructor(props){
-    super(props);
-
-}
-render () {
+const Hot = () => {
+    const todos = useSelector(selectMem)
     return (
-        <div>
+            <div> 
+              {
+        todos.map(item => (<div><Mem path={item.path} upvotes={item.upvotes} downvotes={item.downvotes} name={item.name}/></div>))
+      }
+            </div>
         
-        </div>
     )
 }
-}
+
+export default Hot

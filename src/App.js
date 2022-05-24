0c7e6de40738ad4b7b8c809aa8e30ce82b668ab9
Marszuk <mariola@szukielojc.com>
2022-05-24 21:18:43 +0200

    ch10

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import './App.css';
-import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Regular from './components/Regular';
-import Hot from './components/Hot';
-import Page404 from './components/Page404';
-import Navigation from './components/Navigation';
-import Mem from './components/Mem'
-import { useSelector } from 'react-redux';
-import { selectMem } from './store/reducers/globalReducer';
-import Input from './components/Input';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Regular from "./components/Regular";
+import Hot from "./components/Hot";
+import Page404 from "./components/Page404";
+import Navigation from "./components/Navigation";
+import Input from "./components/Input";
 
 function App() {
- 
-  return( <div className="App">
-    <BrowserRouter>
-    <Navigation/>
-    <Routes>
-      <Route path="/" element={<Regular/>}></Route>
-      <Route path="/Hot" element={<Hot/>}></Route>
-      <Route path="/input" element={<Input/>}></Route>
-      <Route path="*" element={<Page404/>}></Route>
-    </Routes>
-    </BrowserRouter>
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Regular />}></Route>
+          <Route path="/Hot" element={<Hot />}></Route>
+          <Route path="/input" element={<Input />}></Route>
+          <Route path="*" element={<Page404 />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
